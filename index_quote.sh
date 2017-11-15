b6ddd97c47f0f3bbe48cc7e29652ae1e6cc19086
Richard Chan <richard@HomeMacmini.local>
2017-11-15 23:06:40 +0800

    index_quote.sh include aastock index quote

--- index_quote.sh
+++ index_quote.sh
@@ -1,5 +1,5 @@
-#echo "from yahoo:"
-#curl "http://download.finance.yahoo.com/d/quotes.csv?s=^HSI+^HSCE+^HSIL&f=snl1c"
+echo "from aastocks:"
+curl -s "http://www.aastocks.com/tc/resources/datafeed/getstockindex.ashx?type=1" | sed -e 's/{/\'$'\n{/g' | sed -e 's/}/}\'$'\n/g' | grep '^{'
 
 echo "from bloomberg:"
 echo "------HSI-------"
@@ -7,3 +7,4 @@ curl -s "https://www.bloomberg.com/markets/api/quote-page/HSI:IND" | sed -e 's/\
 
 echo "-----HSCEI-----"
 curl -s "https://www.bloomberg.com/markets/api/quote-page/HSCEI:IND" | sed -e 's/\"price/\'$'\n\"price/g' | sed -e 's/\"percent/\'$'\n"percent/g' | grep -m 3 '^\"price\|^\"percent'
+
