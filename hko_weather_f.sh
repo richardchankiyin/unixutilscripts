ed42b09c806a51ac5df258859f8c94bb8ee76a8d
Richard Chan <richard@HomeMacmini.local>
2018-01-05 22:54:24 +0800

    hko_weather_f.sh add to 8 lines in grep

--- hko_weather_f.sh
+++ hko_weather_f.sh
@@ -7,6 +7,6 @@ do
  header="$(echo "$content" | grep -m$x '^Date\/Month' | tail -1)"
  echo  $header
  echo "-----------------------------------"
- forecast=$(echo "$content" | grep -A 6 "$header")
+ forecast=$(echo "$content" | grep -A 8 "$header")
  echo $forecast  $cartoon
 done
