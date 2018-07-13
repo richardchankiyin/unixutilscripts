d51c76e4e736a452ee84d2ba975b0992ec383826
Ngan Home <nganhome@187-115-16-223-on-nets.com>
2018-07-13 10:53:17 +0800

    index_quote.sh remove bloomberg quote

--- index_quote.sh
+++ index_quote.sh
@@ -1,10 +1,3 @@
 echo "from aastocks:"
 curl -s "http://www.aastocks.com/tc/resources/datafeed/getstockindex.ashx?type=1" | sed -e 's/{/\'$'\n{/g' | sed -e 's/}/}\'$'\n/g' | grep '^{'
 
-echo "from bloomberg:"
-echo "------HSI-------"
-curl -s "https://www.bloomberg.com/markets/api/quote-page/HSI:IND" | sed -e 's/\"price/\'$'\n\"price/g' | sed -e 's/\"percent/\'$'\n"percent/g' | grep -m 3 '^\"price\|^\"percent'
-
-echo "-----HSCEI-----"
-curl -s "https://www.bloomberg.com/markets/api/quote-page/HSCEI:IND" | sed -e 's/\"price/\'$'\n\"price/g' | sed -e 's/\"percent/\'$'\n"percent/g' | grep -m 3 '^\"price\|^\"percent'
-
