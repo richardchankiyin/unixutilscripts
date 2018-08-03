3c047cadef6d8c781d96cbeb6de6832c2a14d918
Ngan Home <nganhome@187-115-16-223-on-nets.com>
2018-08-03 09:21:56 +0800

    stock_quote.sh supporting multiple codes

--- stock_quote.sh
+++ stock_quote.sh
@@ -1,9 +1,14 @@
-s=$1 
-#s1=$(curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=$s&f=snl1")
-#echo "from yahoo:$s1"
+#!/bin/bash
+
+for s in "$@"
+do
+echo "------------------------"
+echo "processing $s..........."
 s2=$(echo $s | cut -d'.' -f1 | sed 's/^0*//')
 s3=$(printf "%05d" $s2)
 schi=$(curl -s http://www.aastocks.com/tc/ltp/rtquote.aspx?symbol=$s3 | grep "HK Stock Free" | awk -F '&nbsp;' '{print $2 $1 $3}')
 echo "from aastock:$schi"
 saaq=$(curl -s http://www.aastocks.com/tc/resources/datafeed/getrtqsymbol.ashx?s=$s3)
 echo "from aastock:$saaq"
+done
+
