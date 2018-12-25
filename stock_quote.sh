28513d7700ada41c4307723dad2c76c34ddbf14f
richardchankiyin <richard.chan@analyzingstocks.com>
2018-12-25 12:03:51 +0800

    stock_quote.sh update due to aastocks revamped the page

--- stock_quote.sh
+++ stock_quote.sh
@@ -6,7 +6,7 @@ echo "------------------------"
 echo "processing $s..........."
 s2=$(echo $s | cut -d'.' -f1 | sed 's/^0*//')
 s3=$(printf "%05d" $s2)
-schi=$(curl --connect-timeout $timeout -s http://www.aastocks.com/tc/ltp/rtquote.aspx?symbol=$s3 | grep "HK Stock Free" | awk -F '&nbsp;' '{print $2 $1 $3}')
+schi=$(curl --connect-timeout $timeout -s "http://www.aastocks.com/tc/stocks/quote/frame-quote.aspx?symbol=$s3" -H "Referer: http://www.aastocks.com/tc/stocks/quote/quick-quote.aspx?symbol=$s3&S=Y" |  sed -e 's/var/\'$'\nvar/g' | grep "^var \_Quote" | cut -d',' -f1 | cut -d':' -f2 | sed 's/"//g')
 echo "from aastock:$schi"
 saaq=$(curl --connect-timeout $timeout -s http://www.aastocks.com/tc/resources/datafeed/getrtqsymbol.ashx?s=$s3)
 echo "from aastock:$saaq"
