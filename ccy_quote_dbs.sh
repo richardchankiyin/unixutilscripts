9fc29d3cbfb63d8dce1cc33cea490fc042670f18
Ricahrd Chan <richard.chan@analyzingstocks.com>
2020-06-25 21:25:15 +0800

    update ccy_quote_dbs.sh as dbs changed to api json call on fx rates

--- ccy_quote_dbs.sh
+++ ccy_quote_dbs.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then echo "usage: ./ccy_quote_dbs.sh <ccy> <hkd/usd>"; ccy1="cnyonshore"; ccy2="usd"; else ccy1="$1"; ccy2="$2"; fi
+if [ "$#" -lt 2 ]; then echo "usage: ./ccy_quote_dbs.sh <ccy1> (<ccy2> <ccy3> ...) <hkd/usd>"; exit -1; fi
 
-# as dbs does not support cnh/cny, a conversion should be done
-if [ "$ccy1" == "cnh" ]; then ccy1="cnyoffshore"; fi
-if [ "$ccy1" == "cny" ]; then ccy1="cnyonshore"; fi
+againstccy="${!#}"
+#echo "${againstccy^^}"
+againstccy="$(echo "$againstccy" | awk '{print toupper($0)}')"
+if [ "${againstccy}" == "HKD" ]; then againstccy="HKD"; else againstccy="USD"; fi
 
-content="$(curl -s https://hk.dbs.com/ecPortalWeb/Rates/ExchangeRate.aspx?Lang=en-us)"
+#echo "$againstccy"
+n=$(($# - 2))
+#echo $n
 
-lastupcontent="$(echo "$content" | sed -e 's/<\/span>/\'$'\n<\/span>/g' | grep 'id="lblLastUpdate"' | sed 's/<span class="time-stamp">//;s/<span id="lblLastUpdate">//' )"
+c="";
+for ((i=1; i<=$n; i++)); do
+   c+="${!i}\|";
+done
+#echo "$c"
+m=$(($# - 1))
+#echo "$m"
+c+="${!m}"
+#echo "$c"
 
-echo $lastupcontent
-
-echo "currency pair: $ccy1/$ccy2"
-
-ratescontent="$(echo "$content" | sed -e 's/<\/table>/\'$'\n<\/table>/g' | grep -i "id='$ccy1'" | sed -e 's/<td/\'$'\n<td/g' | grep '<p>[0-9]*.[0-9]*</p>' | awk -F '<p>' '{print $2}' | awk -F '</p>' '{print $1}')"
-
-#echo $ratescontent
-if [ "$ccy2" = "hkd" ]; then echo $ratescontent | sed 's/ /\'$'\n/g' | head -2; else echo $ratescontent | sed 's/ /\'$'\n/g' | tail -2; fi 
-
+if [ $againstccy == "HKD" ]; then 
+curl -s "https://www.dbs.com.hk/hk-rates-api/v1/api/hkrates/latestForexRates" | sed 's/{/\'$'\n/g' | sed 's/}/\'$'\n/g' | grep -i "$c" | awk -F ',|:' '{print $2,$6,$4}' | sed 's/"//g'
+else
+curl -s "https://www.dbs.com.hk/hk-rates-api/v1/api/hkrates/latestForexRates" | sed 's/{/\'$'\n/g' | sed 's/}/\'$'\n/g' | grep -i "$c" | awk -F ',|:' '{print $2,$8,$10}' | sed 's/"//g'
+fi
 
