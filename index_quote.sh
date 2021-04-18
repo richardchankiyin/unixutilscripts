e7aa7146cf523e50e8b7ef2ab70908180acbf779
richard <richardchankiyin@gmail.com>
2021-04-18 10:53:04 +0800

    use compressed for index_quote.sh

--- index_quote.sh
+++ index_quote.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 timeout=5
 echo "from aastocks:"
-curl --connect-timeout $timeout -s "http://www.aastocks.com/tc/resources/datafeed/getstockindex.ashx?type=1" | sed -e 's/{/\'$'\n{/g' | sed -e 's/}/}\'$'\n/g' | grep '^{'
+curl --connect-timeout $timeout -s "http://www.aastocks.com/tc/resources/datafeed/getstockindex.ashx?type=1" --compressed | sed -e 's/{/\'$'\n{/g' | sed -e 's/}/}\'$'\n/g' | grep '^{'
 
