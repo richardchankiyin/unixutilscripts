2f88eb526f8432e260d53b2acf5e771320928f1d
Richard Chan <richardchankiyin@gmail.com>
2022-09-21 06:03:50 +0800

    fix publicholiday.sh due to html style change

--- publicholiday.sh
+++ publicholiday.sh
@@ -4,4 +4,4 @@ year="$(date +%Y)"
 if [ "$#" -gt 0 ];then year="$1"; fi
 
 echo "public holiday in Year $year"
-curl -s https://www.gov.hk/en/about/abouthk/holiday/$year.htm | grep 'class="date"' | sed 's/<td class="date" width="33%">//g;s/<\/td>//g;s/&nbsp;/ /g;s/^ *//g'| tail -n +2
+curl -s https://www.gov.hk/en/about/abouthk/holiday/$year.htm | grep 'class="date"' | sed 's/<td class="date" style="height: 18px;" width="33%">//g;s/<\/td>//g;s/&nbsp;/ /g;s/^ *//g'| tail -n +2
